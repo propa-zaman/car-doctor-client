83d9d572c3eedae711fad3d01c30d28fa7976030
Afra <propazaman12@gmail.com>
2023-05-19 16:57:54 +0600

    bookService

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import Main from '../Layout/Main';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import SignUp from '../Pages/SignUp/SignUp';
+import BookService from '../Pages/BookService/BookService';
+
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,11 @@ const router = createBrowserRouter([
       {
         path: "signup",
         element: <SignUp></SignUp>,
+      },
+      {
+        path: "/book/:id",
+        element: <BookService></BookService>,
+        loader: ({params}) => fetch(`http://localhost:5002/services/${params.id}`)
       },
     ]
   },
