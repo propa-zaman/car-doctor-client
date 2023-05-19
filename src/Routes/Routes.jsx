be8512fe48af1faa0481656932e71e2eeabb6de7
Afra <propazaman12@gmail.com>
2023-05-19 18:00:43 +0600

    final commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,8 @@ import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import SignUp from '../Pages/SignUp/SignUp';
 import BookService from '../Pages/BookService/BookService';
+import Bookings from '../Pages/Bookings/Bookings';
+import PrivateRoute from './PrivateRoute';
 
 
 const router = createBrowserRouter([
@@ -27,6 +29,10 @@ const router = createBrowserRouter([
         path: "/book/:id",
         element: <BookService></BookService>,
         loader: ({params}) => fetch(`http://localhost:5002/services/${params.id}`)
+      },
+      {
+        path: "bookings",
+        element: <PrivateRoute><Bookings></Bookings></PrivateRoute>,
       },
     ]
   },
